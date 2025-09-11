2827a9602c450e01115632350ab79237ca892d25
Karloz Craxker <caguacas.3749@unicesmag.edu.co>
2025-09-11 10:40:48 -0500

    Password encrypted

--- src/signup.php
+++ src/signup.php
@@ -10,6 +10,8 @@
     $e_mail     =   $_POST['email'];
     $p_wd       =   $_POST['passwd'];
 
+    $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+
     //Step 3. Create query to insert into
     $query = "
         INSERT INTO users(
@@ -25,7 +27,7 @@
             '$m_number', 
             '$ide_number', 
             '$e_mail', 
-            '$p_wd')";
+            '$enc_pass')";
 
     //Step 4. Execute query
     $res= pg_query($conn, $query);
